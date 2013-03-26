fd017fdd8b4d2900d89aa923829ae9c488d6cbdc
Dennis Honeyman <arcticuno@gmail.com>
2013-03-26 23:53:00 +0000

    Add script file for maze parsing.

---
+++ read_maze.py
@@ -0,0 +1,5 @@
+
+def read_maze(filename):
+    '''Returns the maze as a 2D array of booleans, the starting coordinates,
+    and the goal coordinates. In the array, True represents walkable.'''
+    pass
