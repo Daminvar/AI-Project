e44dc120335f3b69341f68e54fa5b557ec1be8b7
Dennis Honeyman <arcticuno@gmail.com>
2013-03-27 15:18:25 +0000

    Add Manhattan distance heuristic.

--- heuristics.py
+++ heuristics.py
@@ -2,3 +2,12 @@
 #
 # Authors: Sean Mullen
 # 	
+
+def manhattanDistance(start, end):
+    return abs((end[0] - start[0]) + (end[1] - start[1]))
+
+def directionHeuristic(start, end):
+    pass
+
+def unknownHeuristic(start, end):
+    pass
