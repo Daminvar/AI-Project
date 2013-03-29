12484f77dabd66561ceaa44f2759401b2174439b
Sean <smullen@ubuntu.ubuntu-domain>
2013-03-29 00:43:06 -0400

    Updated minor bug and tested.

--- read_maze.py
+++ read_maze.py
@@ -1,36 +1,38 @@
 
 def read_maze(filename):
-    '''Returns the maze as a 2D array of booleans, the starting coordinates,
-    and the goal coordinates. In the array, True represents walkable.'''
-    maze = []
-    start = None
-    goal = None
-    
-    # gets file handler
-    fileInput = open(filename, 'r')
-    
-    index = 0
-    for line in fileInput:
-    	maze.append(line)
-    	
-    	if (start == None):
-    		temp = find(line, 'S')
-    		
-    		# create start tuple in Y, X
-    		start = (temp, index)
-    	
+	'''Returns the maze as a 2D array of booleans, the starting coordinates,
+	and the goal coordinates. In the array, True represents walkable.'''
+	maze = []
+	start = None
+	goal = None
+	
+	# gets file handler
+	fileInput = open(filename, 'r')
+	
+	index = 0
+	for line in fileInput:
+		maze.append(line)
+		
+		if (start == None):
+			temp = find(line, 'S')
+			
+			if temp > -1:
+				# create start tuple in Y, X
+				start = (temp, index)
+		
 		if (goal == None):
-    		temp = find(line, 'G')
-    		
-    		# create goal tuple in Y, X
-    		goal = (temp, index)
-    		
-    	index += 1
-    
-    
-    return start, goal, maze
-    
-    
+			temp = find(line, 'G')
+			
+			if temp > -1:
+				# create goal tuple in Y, X
+				goal = (temp, index)
+			
+		index += 1
+	
+	
+	return start, goal, maze
+	
+	
 def find(source, search):
 	i = 0
 	
@@ -41,4 +43,3 @@ def find(source, search):
 		i += 1
 	
 	return -1
-			
