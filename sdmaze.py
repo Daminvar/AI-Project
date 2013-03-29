a562afacf86ac2aacad6cfed5e912d4c68039a64
Sean <smullen@ubuntu.ubuntu-domain>
2013-03-29 19:36:09 -0400

    Started adding functionality to run maze program.

--- sdmaze.py
+++ sdmaze.py
@@ -1,12 +1,26 @@
-def main():
-    pass
-    
-    
-    
-def display():
-    pass
-    
+from read_maze import *
+from die import *
+from heuristics import *
+import sys
 
+def main():
+    if len(sys.argv) != 2:
+        print "usage: python sdmaze.py maze_file"
+        return
+
+    print 'Reading in ' + sys.argv[1] + '...\n'
+    data = read_maze(sys.argv[1])
+    
+    if data[0] == None or data[1] == None:
+        print "Invalid board configuration. Board must have start and stop goals."
+        return;
+    
+    die = Die(data[0], data[1], data[2], None, 1, 2, 3)
+    
+    print 'Initial board state is: '
+    print die
+    
+    
 if __name__ == '__main__':
     main()
     
