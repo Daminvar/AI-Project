5a44c616ddd37b9cda788734fff90cbabcad0bcb
Dennis Honeyman <arcticuno@gmail.com>
2013-03-30 02:19:33 +0000

    Add performance metrics.

--- heuristics.py
+++ heuristics.py
@@ -6,7 +6,7 @@ import math
 
 
 def manhattanDistance(die):
-    return abs((die.goal[0] - die.start[0]) + (die.goal[1] - die.start[1]))
+    return abs(die.goal[0] - die.start[0]) + abs(die.goal[1] - die.start[1])
 
 def straightLineDistance(die):
     '''We can change this to a different heuristic if we want.'''
