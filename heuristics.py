a7131b8303e37ceb5864a53603d98c21cf63c13e
Dennis Honeyman <arcticuno@gmail.com>
2013-03-30 03:13:40 +0000

    Remove TODO comment.

--- heuristics.py
+++ heuristics.py
@@ -11,7 +11,6 @@ def manhattanDistance(die):
     return abs(die.goal[0] - die.start[0]) + abs(die.goal[1] - die.start[1])
 
 def straightLineDistance(die):
-    '''We can change this to a different heuristic if we want.'''
     return math.sqrt((die.goal[0] - die.start[0])**2 + (die.goal[1] - die.start[1])**2)
 
 
